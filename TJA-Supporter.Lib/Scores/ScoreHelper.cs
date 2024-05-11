91ac08efcaafc4c5affec4b68f279d9d63a87662
SHiNKi <shinki.5ktri@gmail.com>
2024-05-11 13:43:33 +0900

    元の譜面のスクロール速度を維持したマージを実装

--- TJA-Supporter.Lib/Scores/ScoreHelper.cs
+++ TJA-Supporter.Lib/Scores/ScoreHelper.cs
@@ -57,5 +57,49 @@ namespace TJA_Supporter.Lib.Scores
             }
             return mergedScore;
         }
+
+        /// <summary>
+        /// マージする譜面の速度はそのままに、譜面をマージします。
+        /// </summary>
+        /// <param name="baseBpm">ベースとなるBPM</param>
+        /// <param name="baseBeat">ベースとなる拍子</param>
+        /// <param name="baseNotesSize">1小節当たりの休符含むノーツ数</param>
+        /// <param name="scores">マージする譜面</param>
+        /// <returns></returns>
+        public static Score NearMergeConsideringScroll(double baseBpm, Fraction baseBeat, int baseNotesSize, params Score[] scores)
+        {
+            List<Measure> measures = new();
+
+            // 尺が長い方に合わせる
+            double maxLength = scores.Select(t => t.Length).Max();
+
+            // 最低何小節必要か求める
+            double measureCountDbl = (maxLength * baseBpm) / (240 * baseBeat.Value);
+            int measureCount = (int)System.Math.Round(measureCountDbl, 0);
+            // 求めた小節数分 空の小節を生成する
+            for (int i = 0; i < measureCount; i++)
+                measures.Add(Measure.CreateBlank(baseBpm, baseBeat, baseNotesSize));
+
+            Score mergedScore = new(measures);
+            foreach (var scoreInfo in scores)
+            {
+                // 小節の列挙
+                int i_measure = 0;
+                foreach (var measure in scoreInfo.Measures)
+                {
+                    double totalLength = scoreInfo.GetLengthUntil(i_measure);
+                    // ノーツと長さの列挙（休符は対象外）
+                    foreach (var noteAndLength in measure.EnumerateNotesAndLength().Where(n => n.Item1.Type != NoteType.None))
+                    {
+                        var offsetAndIndex = mergedScore.FirstMeasure!.Value.GetNearIndex(0, totalLength + noteAndLength.Item2);
+                        Note note = mergedScore[offsetAndIndex.MeasureOffset][offsetAndIndex.Index];
+                        note.Type = noteAndLength.Item1.Type;
+                        note.Scroll = noteAndLength.Item1.Scroll * (measure.BPM / baseBpm);
+                    }
+                    i_measure++;
+                }
+            }
+            return mergedScore;
+        }
     }
 }
